948d3f78244ab1cc1569044fdb4807fd94d8cca0
João Ferreira <joaoprcf@ua.pt>
2022-03-04 10:16:46 +0000

    added video links to markdown

--- README.md
+++ README.md
@@ -1,29 +1,8 @@
 # PSR-TP3-G16
 Trabalho de Grupo TP3 de PSR - Grupo 16
 
-Parâmetros de avaliação:
 
-
-- Ficheiros Descrição do Robô
-- Launch files de lançamento do robô
-- Simulação em Gazebo
-- Visualização Robô em RViz
-- Manual driving race (Teleop)
-- Seguimento Goal RViz
-- Interface do nó driver
-- Perceção da cena
-- Modo de Fuga
-- Modo de Perseguição
-- Evitar obstáculos
-- Mapeamento do cenário
-- Variabilidade Drivers
-- Demonstração do jogo 
-- Extras 
-- Repositório Github
-
-
-
-[Vídeo team hunt](https://youtu.be/a2rEA_lOTcg "G16 PSR 2022")
-
+[Vídeo team hunt](https://youtu.be/a2rEA_lOTcg "G16 PSR 2022, team hunt")
+[Vídeo teleop](https://youtu.be/BkVwjSxXK1M "G16 PSR 2022, teleop")
 
 
--- g16_player/src/supervised_lerning.py
+++ g16_player/src/supervised_lerning.py
@@ -1,8 +1,29 @@
 #!/usr/bin/env python3
 import numpy as np
 
+import tensorflow as tf
+import numpy as np
+from keras import layers as l
+from keras.layers import Input, Dense, Conv2D, MaxPooling2D, UpSampling2D, Reshape, Conv2DTranspose, BatchNormalization, Dropout, Flatten, concatenate
+from keras.models import Model
+from keras.utils import plot_model, get_file
+import pandas as pd
+from keras.preprocessing.image import ImageDataGenerator
+import matplotlib.pyplot as plt
 
+
+# supervised learning model 
 
 a = np.load("inputs.npy")
+b = np.load("outputs.npy")
 
-print(a.shape)
+inputs = Input(shape=(25,))
+x = Dense(20, activation='relu')(inputs)
+outputs = Dense(2, activation='linear')(x)
+
+model = Model(inputs=inputs, outputs=outputs)
+model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['acc'])
+
+model.fit(a, b, )
+
+print(a.shape, b.shape)
